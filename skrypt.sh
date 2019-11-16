3a3c02ca546e5965c94fcf19a7ad81fee0323cff
s21790-pj <56189587+s21790-pj@users.noreply.github.com>
2019-11-16 08:58:28 +0100

    Final

--- skrypt.sh
+++ skrypt.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-wget --quiet -O - 'https://skm.trojmiasto.pl/rozklad/?from=7500&to=7534'
+wget --quiet -O - 'https://skm.trojmiasto.pl/rozklad/?from=7500&to=7534' | grep - P '[0-9]:[0-9]' | sort | uniq
 
 
